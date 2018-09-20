ea6b42b16103885d3813cb4dd59834f8951f9618
Héctor I. Rubio A <hirasyscom@gmail.com>
2018-09-20 07:16:34 -0400

    Add files via upload

---
+++ hrtodolist/src/pages/Login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ScrollView, Text, TextInput, View, Button } from 'react-native';
+import { login } from '../actions/auth';
+
+class Login extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    userLogin (e) {
+        this.props.onLogin(this.state.username, this.state.password);
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <ScrollView style={{padding: 20}}>
+                <Text style={{fontSize: 27}}>
+                    Usuario:
+                </Text>
+                <TextInput 
+                    placeholder='Escriba su nombre de usuario' 
+                    autoCapitalize='none' 
+                    autoCorrect={false} 
+                    autoFocus={true} 
+                    keyboardType='email-address' 
+                    value={this.state.username} 
+                    onChangeText={(text) => this.setState({ username: text })} />
+                <TextInput 
+                    placeholder='Escriba su contraseña' 
+                    autoCapitalize='none' 
+                    autoCorrect={false} 
+                    secureTextEntry={true} 
+                    value={this.state.password} 
+                    onChangeText={(text) => this.setState({ password: text })} />
+                <View style={{margin: 7}}/>
+                <Button onPress={(e) => this.userLogin(e)} title="Entrar"/>
+            </ScrollView>
+        );
+    }
+}
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        isLoggedIn: state.auth.isLoggedIn
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogin: (username, password) => { dispatch(login(username, password)); }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
